4ce7e26d8c4702755de0ad4cc56b7c3fee739c77
budhitejaswi <87587371+budhitejaswi@users.noreply.github.com>
2022-01-08 19:27:01 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:' , ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8CACVsjsO/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded! :)');
+}
+
+function check() {
+
+    captured_image = document.getElementById("captured_image");
+    classifier.classify(captured_image, gotResults);
+}
+
+function gotResults(error, results){
+
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML= results[0].label;
+
+        prediction = results[0].label;
+        speak();
+
+        if(results[0].label == "Best")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+
+        if(results[0].label == "Amazing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+
+        if(results[0].label == "Clapping")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128079;";
+        }
+
+        if(results[0].label == "Rock On!")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#129311;";
+        }
+
+    }
+}
+
+function speak(){
+    var synth= window.speechSynthesis;
+    speak_data ="The prediction is " +prediction;
+    var utterThis = new SpeechSynthesisUtterance (speak_data);
+    synth.speak(utterThis);
+}
